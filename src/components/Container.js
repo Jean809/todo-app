cc880fe22af9d10f9d73e1371748cedebf3d0b32
Jean809 <jean_carlosrosario709@outlook.com>
2023-01-02 21:35:25 -0500

    added useState for transition of Notifications to Todo component.

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,16 +2,38 @@ import React, { useState } from "react";
 import ContainerCSS from "./Container.module.css";
 
 function Container() {
-  const [containerIsEmpty, setContainerIsEmpty] = useState(true);
+  const [todoIsActive, setTodoIsActive] = useState(true);
+
+  function setStateToFalse() {
+    setTodoIsActive(false);
+  }
+
+  function setStateToTrue() {
+    setTodoIsActive(true);
+  }
 
   return (
     <div>
       <header>
-        <h2 className={`${ContainerCSS.notif}`}>Notifications</h2>
-        <h2 className={`${ContainerCSS.todo}`}>Todo</h2>
+        <h2
+          onClick={setStateToFalse}
+          className={`${ContainerCSS.notif} ${
+            todoIsActive ? ContainerCSS.inactiveBar : ContainerCSS.activeBar
+          }`}
+        >
+          Notifications
+        </h2>
+        <h2
+          onClick={setStateToTrue}
+          className={`${ContainerCSS.todo} ${
+            todoIsActive ? ContainerCSS.activeBar : ContainerCSS.inactiveBar
+          }`}
+        >
+          Todo
+        </h2>
       </header>
       <div className={`${ContainerCSS.emptyContainer}`}>
-        <h2 className={`${containerIsEmpty}`}>Add a task to display here</h2>
+        <h2>Add a task to display here</h2>
       </div>
     </div>
   );
